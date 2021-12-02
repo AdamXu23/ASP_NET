d4eca1678ccff6d3dc88cb0278f0989ea550742b
AdamXu <xsz1993@live.com>
2021-12-02 19:20:30 +0800

    20211202_1920

---
+++ ASP_NET_Core/Chapter02/CoreMvc3_Identity/CoreMvc3_Identity/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CoreMvc3_Identity.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using System.Runtime.InteropServices;
+
+namespace CoreMvc3_Identity.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            if (User.Identity.Name != "Kevin@gmail.com")
+            {
+                return Content($"{User.Identity.Name}無權存取此Action動作方法!");
+            }
+
+            return View();
+        }
+
+        [Authorize(Roles = "Admin, Supervisor")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP_NET_Core/Chapter02/CoreMvc3_firstMVC/CoreMvc3_firstMVC/Models/Friend.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMvc3_firstMVC.Models
+{
+    public class Friend
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+}
