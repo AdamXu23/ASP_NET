66301296a811d2d510345aa99e896a6b858f74d7
AdamXu <xsz1993@live.com>
2021-12-03 10:21:47 +0800

    20211203-1021

---
+++ ASP_NET_Core/Chapter02/CoreMvc3_Identity_TEST_2022/CoreMvc3_Identity_TEST_2022/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoreMvc3_Identity_TEST_2022.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
