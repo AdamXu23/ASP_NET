fcf90e9fdc696a40ac28bec28365bfe8d932e915
AdamXu <xsz1993@live.com>
2021-12-05 21:35:44 +0800

    20211205-9035

--- ASP_NET_Core/Chapter02/CoreMvc3_Identity_TEST_2022/CoreMvc3_Identity_TEST_2022/Controllers/HomeController.cs
+++ ASP_NET_Core/Chapter02/CoreMvc3_Identity_TEST_2022/CoreMvc3_Identity_TEST_2022/Controllers/HomeController.cs
@@ -1,11 +1,15 @@
 ﻿using CoreMvc3_Identity_TEST_2022.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+
 
 namespace CoreMvc3_Identity_TEST_2022.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
+        
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -13,6 +17,7 @@ namespace CoreMvc3_Identity_TEST_2022.Controllers
             _logger = logger;
         }
 
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
@@ -20,10 +25,14 @@ namespace CoreMvc3_Identity_TEST_2022.Controllers
 
         public IActionResult Privacy()
         {
+            if(User.Identity.Name != "Kevin@gmail.com")
+            {
+                return Content($"{User.Identity.Name}無權力存取此Action動作方法！");
+            }
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Authorize(Roles ="Admin,Supervisor")]
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
