d68936b9602cb6971bc797177df8ba7494f13541
AdamXu <xsz1993@live.com>
2021-12-09 22:19:50 +0800

    20211209-2019

--- ASP_NET_Core/Chapter03/McvFriends/Data/DatabaseContext.cs
+++ ASP_NET_Core/Chapter03/McvFriends/Data/DatabaseContext.cs
@@ -1,3 +1,4 @@
+using System.Reflection.Emit;
 using Microsoft.EntityFrameworkCore;
 using McvFriends.Models;
 namespace McvFriends.Data
@@ -6,6 +7,15 @@ namespace McvFriends.Data
     {
         public DatabaseContext(DbContextOptions<DatabaseContext> options):
             base(options){}
-            public DbSet<Friend> Friends{get;set;}
+        public DbSet<Friend> Friends{get;set;}
+        protected override void OnModelCreating(ModuleBuilder modulebuilder)
+        {
+            modulebuilder.Entity<Friend>().HasData(
+                new Friend{id = 1 ,Name="Mary",Email="Mary@gmail.com",Mobile="0922-355822"},
+                new Friend{id = 2 ,Name="David",Email="David@gmail.com",Mobile="0933-123456"},
+                new Friend{id = 3 ,Name="Rose",Email="Rose@gmail.com",Mobile="0955-888-163"}
+            );
+        }
     }
+    
 }
