6b1eb9b63629b372f4e38c455264d545041cd825
AdamXu <xsz1993@live.com>
2021-12-09 22:01:33 +0800

    20211209-1001

--- ASP_NET_Core/Chapter03/McvFriends/EF Core Migration.bat
+++ ASP_NET_Core/Chapter03/McvFriends/EF Core Migration.bat
@@ -1,2 +1,3 @@
 dotnet ef migrations add InitialDB
 dotnet ef database update
+dotnet aspnet-codegenerator controller --controllerName FriendsController -outDir Controllers -async -namespace MvcFriends.Controllers -m Friend -dc DatabaseContext -udl
