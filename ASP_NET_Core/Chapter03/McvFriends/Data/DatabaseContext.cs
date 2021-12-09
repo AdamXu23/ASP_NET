907e8bdc58b214d7b0338313bbbf0ea2e92b0619
AdamXu <xsz1993@live.com>
2021-12-09 19:39:37 +0800

    Update DatabaseContext.cs

--- ASP_NET_Core/Chapter03/McvFriends/Data/DatabaseContext.cs
+++ ASP_NET_Core/Chapter03/McvFriends/Data/DatabaseContext.cs
@@ -2,5 +2,10 @@ using Microsoft.EntityFrameworkCore;
 using McvFriends.Models;
 namespace McvFriends.Data
 {
-    
+    public class DatabaseContext:DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options):
+            base(options){}
+            public DbSet<Friend> Friends{get;set;}
+    }
 }
