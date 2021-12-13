1cbc783b967290c735ad1d5fc0e360113f5d6ae4
AdamXu <xsz1993@live.com>
2021-12-13 13:24:54 +0800

    20211213-1135

--- ASP_NET_Core/Chapter04/Fundamentals_TEST_2022/Fundamentals_TEST_2022/Program.cs
+++ ASP_NET_Core/Chapter04/Fundamentals_TEST_2022/Fundamentals_TEST_2022/Program.cs
@@ -1,10 +1,52 @@
-using Fundamentals_TEST_2022.Data;
+﻿using Fundamentals_TEST_2022.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
+
+
 var builder = WebApplication.CreateBuilder(args);
 
+Dictionary<string, string> dictEmployees = new Dictionary<string, string>
+        {
+                {"Asia:employees:1", "Mary"},
+                {"Asia:employees:2", "John"},
+                {"Asia:employees:3", "Kevin"},
+                {"Asia:employees:4", "David"},
+                {"Asia:employees:5", "Rose"}
+        };
 
+//Set host configuration
+
+builder.Host.ConfigureHostConfiguration(configHost =>
+{
+    //設定基底路徑
+    configHost.SetBasePath(Directory.GetCurrentDirectory());
+
+    //configHost有Properties和Sources兩個屬性,前者是Key(FileProvide)及Value(PhysicalFileProvide),後者是Providers提供者資訊
+    //如想看見SetBasePath()設定後路徑,可用下面程式抓取
+    string contentRoot = ((Microsoft.Extensions.FileProviders.PhysicalFileProvider)configHost.Properties.Values.FirstOrDefault()).Root;
+
+    configHost.AddJsonFile("hostsettings.json", optional: true);
+    configHost.AddEnvironmentVariables(prefix: "PREFIX_");
+    configHost.AddCommandLine(args);
+
+});
+
+builder.Host.ConfigureAppConfiguration((hostingContext, configApp) =>
+{
+    //configApp.AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json"), true, true);
+
+    //設定組能檔完整路徑
+    string path = Path.Combine(Directory.GetCurrentDirectory(), "ConfigFiles");
+    configApp.AddJsonFile(Path.Combine(path, "FutureCorp.json"), optional: true, reloadOnChange: true);  //載入自訂JSON組態檔
+    configApp.AddIniFile(Path.Combine(path, "Mobile.ini"), true, true);      //載入自訂INI組態檔
+    configApp.AddXmlFile(Path.Combine(path, "Computer.xml"), true, true);    //載入自訂XML組態檔
+    configApp.AddJsonFile(Path.Combine(path, "Device.json"), true, true);   //載入自訂JSON組態檔
+
+    path = Path.Combine(Directory.GetCurrentDirectory(), "Configuration");
+    configApp.AddJsonFile(Path.Combine(path, "Food.json"), true, true);
+    configApp.AddInMemoryCollection(dictEmployees);
+});
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
