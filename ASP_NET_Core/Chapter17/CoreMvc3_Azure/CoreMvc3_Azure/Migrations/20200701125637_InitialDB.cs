13f5456a08466d2c0e835d3d098753848d8ed8a7
AdamXu <xsz1993@live.com>
2021-12-13 11:35:17 +0800

    20211213-1135

---
+++ ASP_NET_Core/Chapter17/CoreMvc3_Azure/CoreMvc3_Azure/Migrations/20200701125637_InitialDB.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreMvc3_Azure.Migrations
+{
+    public partial class InitialDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 20, nullable: true),
+                    Mobile = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Department = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
---
+++ ASP_NET_Core/Chapter17/CoreMvc3_Azure/CoreMvc3_Azure/Migrations/20200701125852_AddSeedData.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreMvc3_Azure.Migrations
+{
+    public partial class AddSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employee",
+                columns: new[] { "Id", "Department", "Email", "Mobile", "Name", "Title" },
+                values: new object[,]
+                {
+                    { 1, "總經理室", "david@gmail.com", "0933-152667", "David", "CEO" },
+                    { 2, "人事部", "mary@gmail.com", "0938-456889", "Mary", "管理師" },
+                    { 3, "財務部", "joe@gmail.com", "0925-331225", "Joe", "經理" },
+                    { 4, "業務部", "mark@gmail.com", "0935-863991", "Mark", "業務員" },
+                    { 5, "資訊部", "rose@gmail.com", "0987-335668", "Rose", "工程師" },
+                    { 6, "資訊部", "may@gmail.com", "0955-259885", "May", "工程師" },
+                    { 7, "業務部", "john@gmail.com", "0921-123456", "John", "業務員" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employee",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Employee",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Employee",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Employee",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Employee",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Employee",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Employee",
+                keyColumn: "Id",
+                keyValue: 7);
+        }
+    }
+}
