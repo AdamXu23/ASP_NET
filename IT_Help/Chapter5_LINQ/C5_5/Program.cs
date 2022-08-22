cdb9c6f2c02ff630b8516fa633c3cb5a973e234b
AdamXu <xsz1993@live.com>
2022-08-22 20:17:29 +0800

    dsafghj

--- IT_Help/Chapter5_LINQ/C5_5/Program.cs
+++ IT_Help/Chapter5_LINQ/C5_5/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using C5_5;
+
+var orderby = "Age";
+var Users = new List<User>
+    {
+    new User { Name = "QWE",Age=30,BloodType="O",RegisterTime = new DateTime(2021,05,1) },
+    new User { Name = "ASD",Age=35,BloodType="A" ,RegisterTime = new DateTime(2021,6,2) },
+    new User { Name = "ZXC",Age=20,BloodType="AB" ,RegisterTime = new DateTime(2021,7,3) },
+    new User { Name = "RTY",Age=25,BloodType="A" ,RegisterTime = new DateTime(2021,8,6) },
+    new User { Name = "FGH",Age=31,BloodType="O" ,RegisterTime = new DateTime(2021,10,1) },
+    new User { Name = "VBN",Age=27,BloodType="AB", RegisterTime = new DateTime(2021, 4, 1)},
+    new User { Name = "UIO",Age=30,BloodType="B", RegisterTime = new DateTime(2021, 5, 20)}
+    };
+
+var result = Users.OrderBy(orderby + "descending");
--- IT_Help/Chapter5_LINQ/C5_5/User.cs
+++ IT_Help/Chapter5_LINQ/C5_5/User.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C5_4
+namespace C5_5
 {
     public class User
     {
         public string Name { get; set; }
-        public int age { get; set; }  
+        public int Age { get; set; }  
         public string BloodType { get; set; }
         public DateTime RegisterTime { get; set; }
 
