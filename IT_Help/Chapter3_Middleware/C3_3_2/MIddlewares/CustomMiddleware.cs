ebfe61829875c8413bac8a1368e5a215f69868a8
AdamXu <xsz1993@live.com>
2022-08-22 15:07:57 +0800

    2345

--- IT_Help/Chapter3_Middleware/C3_3_2/MIddlewares/CustomMiddleware.cs
+++ IT_Help/Chapter3_Middleware/C3_3_2/MIddlewares/CustomMiddleware.cs
@@ -3,7 +3,9 @@ namespace SampleMiddleware.Middleware;
 public static class CustomMiddlewareExtetion
 {
     public static void UseCustom( this IApplicationBuilder app)
-    {}
+    {
+        app.UseMiddleware<CustomMiddleware>();
+    }
 }
 
 public class CustomMiddleware
@@ -12,5 +14,12 @@ public class CustomMiddleware
     public CustomMiddleware(RequestDelegate netx)
     {
         _next = netx;
+    }
+    public async Task Invoke (HttpContent context)
+    {
+        await context.Response.WriteAsync("use ext start \r\n");
+        await _next.Invoke(context);
+        await context.Response.WriteAsync("use ext end \r\n");
+        
     }
 }
