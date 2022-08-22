26e99fd8e474aa82f4cff0cfdd27480a7548aad1
AdamXu <xsz1993@live.com>
2022-08-22 20:01:24 +0800

    asda

---
+++ IT_Help/Chapter5_LINQ/C5_1_2_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+List<int> numbers1 = new List<int> { 6, 4, 8, 3, 2, 1, 5, 7, 0, 9 };
+List<int> numbers2 = new List<int> { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+List<int> numbers = new List<int> { 6, 4, 8, 3, 2, 1, 5, 7, 0, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+
+var average = numbers1.Average();
+
+var concatenationQuery = numbers1.Concat(numbers2);
+var largeNumbersQuery = numbers2.Where(c => c > 15);
+
+int numcount1 =
+        (
+        from num in numbers
+        where num < 3 || num > 7
+        select num
+        ).Count();
+IEnumerable<int> numQuery =
+    from num in numbers
+    where num < 3 || num > 7
+    select num;
+int numcount2 = numQuery.Count();
+int i = 123;
---
+++ IT_Help/Chapter5_LINQ/C5_4/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using C5_4;
+
+var Users = new List<User>
+    {
+    new User { Name = "QWE",age=30,BloodType="O" },
+    new User { Name = "ASD",age=35,BloodType="A" },
+    new User { Name = "ZXC",age=20,BloodType="AB" },
+    new User { Name = "RTY",age=25,BloodType="A" },
+    new User { Name = "FGH",age=31,BloodType="O" },
+    new User { Name = "VBN",age=27,BloodType="AB" },
+    new User { Name = "UIO",age=30,BloodType="B" }
+    };
+var result1 = Users.Where(u => u.age > 25);
+var result2 = Users.Where(u => u.age > 25 && u.BloodType == "O");
+var result3 = Users.FirstOrDefault();
+var result4 = Users.FirstOrDefault(u => u.age > 25 && u.BloodType == "AB");
+var result5 = Users.OrderBy(U => U.age);
+var result6 = Users.OrderByDescending(U => U.age);
+var result7 = Users.GroupBy(U => U.BloodType);
+
+int i = 123;
