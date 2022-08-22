40fbc9d7bca91f8c6d4d732bc087479fdbb2243c
AdamXu <xsz1993@live.com>
2022-08-22 20:11:01 +0800

    12346789

---
+++ IT_Help/Chapter5_LINQ/C5_5/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C5_4
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int age { get; set; }  
+        public string BloodType { get; set; }
+        public DateTime RegisterTime { get; set; }
+
+
+    }
+}
