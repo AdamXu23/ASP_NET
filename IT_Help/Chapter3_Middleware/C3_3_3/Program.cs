1de87c1fd42dfa7d520b5a7aaad5fa918325fada
AdamXu <xsz1993@live.com>
2022-08-22 15:34:58 +0800

    214

--- IT_Help/Chapter3_Middleware/C3_3_3/Program.cs
+++ IT_Help/Chapter3_Middleware/C3_3_3/Program.cs
@@ -9,17 +9,55 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.Map("/map1/submap",Map1submap);
+app.Map("/map1",Map1);
+app.Map("/map2",Map2);
+app.Map("/v1",v1);
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.Run(async context =>
+    {
+        await context.Response.WriteAsync("RUN \r\t");
+    });
 
 app.Run();
+
+static void Map1(IApplicationBuilder app)
+{
+    app.Run(async context =>
+    {
+        await context.Response.WriteAsync("Map 1");
+    });
+}
+static void v1(IApplicationBuilder app)
+{
+    app.Map("/v2a",v2a);
+    app.Map("/v2b",v2b);
+}
+static void v2a(IApplicationBuilder app)
+{
+    app.Run(async context =>
+    {
+        await context.Response.WriteAsync("v2a");
+    });
+}
+static void v2b(IApplicationBuilder app)
+{
+    app.Run(async context =>
+    {
+        await context.Response.WriteAsync("v2b");
+    });
+}
+static void Map1submap(IApplicationBuilder app)
+{
+    app.Run(async context =>
+    {
+        await context.Response.WriteAsync("Map 1 / submap");
+    });
+}
+static void Map2(IApplicationBuilder app)
+{
+    app.Run(async context =>
+    {
+        await context.Response.WriteAsync("Map 2");
+    });
+}
