dd581f22c690c1fe5d661ff594296381ff6015b5
AdamXu <xsz1993@live.com>
2022-08-22 19:37:32 +0800

    12346

--- IT_Help/Chapter5_LINQ/Program.cs
+++ IT_Help/Chapter5_LINQ/Program.cs
@@ -3,6 +3,7 @@ Console.WriteLine("Hello, World!");
 
 List<int> numbers1 = new List<int>{6,4,8,3,2,1,5,7,0,9};
 List<int> numbers2 = new List<int>{10,11,12,13,14,15,16,17,18,19};
+List<int> numbers = new List<int>{6,4,8,3,2,1,5,7,0,9,10,11,12,13,14,15,16,17,18,19};
 
 var average = numbers1.Average();
 
@@ -10,8 +11,14 @@ var concatenationQuery = numbers1.Concat(numbers2);
 var largeNumbersQuery = numbers2.Where(c => c>15);
  
 int numcount1 = 
-    (
-        from num in numbers
-     )
-
+        (
+        from num in numbers 
+        where num <3 || num >7
+        select num
+        ).Count();
+IEnumerable<int> numQuery = 
+    from num in numbers
+    where num <3 || num >7
+    select num;
+int numcount2 =  numQuery.Count();
  int i = 123;
